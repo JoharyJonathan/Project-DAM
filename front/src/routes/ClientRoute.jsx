ee05a61f6e27373841ce4aed02195e81bfea6bb0
Mampionona05 <rak.herymampionona@gmail.com>
2024-08-17 17:39:09 +0300

    Ajout des packages

---
+++ front/src/routes/ClientRoute.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom"
+
+const ClientRoute = () => {
+    return (
+        <Routes>
+            <Route />
+        </Routes>
+    )
+}
+
+export default ClientRoute;
